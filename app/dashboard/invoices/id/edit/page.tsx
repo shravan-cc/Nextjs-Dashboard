292e626213cbd675a5fd845534aa395f1d778ab6
Shravan <shravan.hegde@codecraft.co.in>
2024-08-24 16:51:51 +0530

    Implemented chapter 12

---
+++ app/dashboard/invoices/id/edit/page.tsx
@@ -0,0 +1,27 @@
+import Form from '../../../../ui/invoices/edit-form';
+import Breadcrumbs from '../../../../ui/invoices/breadcrumbs';
+import { fetchCustomers,fetchInvoiceById } from '../../../../lib/data';
+import React from 'react';
+ 
+export default async function Page({ params }: { params: { id: string } }) {
+    const id = params.id;
+    const [invoice, customers] = await Promise.all([
+        fetchInvoiceById(id),
+        fetchCustomers(),
+      ]);
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Edit Invoice',
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  );
+}
